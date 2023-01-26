ec26025c8b64709c625de1c346847a5c371318c9
KIMKICHAN <rlcks980705@gmail.com>
2023-01-26 17:52:49 +0900

    feat: couter 컴포넌트 생성

---
+++ components/Stepper.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+function Stepper({ initial, onChange }: Props) {
+  const [count, setCount] = useState(initial);
+
+  const handleIncrement = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+    onChange(newCount);
+  };
+
+  const handleDecrement = () => {
+    const newCount = count - 1;
+    setCount(newCount);
+    onChange(newCount);
+  };
+
+  return (
+    <div>
+      <button data-cy='decrement' onClick={handleDecrement}>
+        -
+      </button>
+      <span data-cy='counter'>{count}</span>
+      <button data-cy='increment' onClick={handleIncrement}>
+        +
+      </button>
+    </div>
+  );
+}
+
+export default Stepper;
+
+interface Props {
+  initial: number;
+  onChange: (count: number) => void;
+}
